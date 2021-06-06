18c04d3b55ee72fb03b36c8c951cc30b26ac7407
rexj8 <rachit.gla_cs18@gla.ac.in>
2021-06-07 04:30:59 +0530

    favicon added

--- demo_streaming/src/App.js
+++ demo_streaming/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import './Components/Navbar.css'
 import './Components/Cards.css'
 
+// import {BrowserRouter as Router , Route , Switch} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">

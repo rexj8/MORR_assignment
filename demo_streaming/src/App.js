a8b202f0e804755c30f9f4b9d5059e3565950eab
rexj8 <rachit.gla_cs18@gla.ac.in>
2021-06-07 00:58:20 +0530

    nav bar completed

--- demo_streaming/src/App.js
+++ demo_streaming/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       {/* <Logo/> */}
-      <Navbar />
+      <Navbar name="Titles"/>
     </div>
   );
 }
--- demo_streaming/src/Components/Navbar.js
+++ demo_streaming/src/Components/Navbar.js
@@ -2,12 +2,21 @@ import React from "react";
 import './Navbar.css'
 
 class Navbar extends React.Component{
+    constructor(props) {
+        super(props);
+    }
     render(){
         return (
-            <div className="navbar">
-                <h1>DEMO Streaming</h1>
-                <a href="#">Log In</a>
-            </div>
+            <>
+                <div className="navbar">
+                    <h1>DEMO Streaming</h1>
+                    <a href="#">Log In</a>
+                    <a href="#" class="free-trial">Start your Free Trial</a>
+                </div>
+                <div className="page-info">
+                    <h3>Popular {this.props.name}</h3>
+                </div>
+            </>
         )
     }
 }

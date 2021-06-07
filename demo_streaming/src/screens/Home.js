da370c38bc6d04b9a60ee4dd9c40994c0b37ff9c
rexj8 <rachit.gla_cs18@gla.ac.in>
2021-06-07 21:24:56 +0530

    done

--- demo_streaming/src/screens/Home.js
+++ demo_streaming/src/screens/Home.js
@@ -13,10 +13,10 @@ function Home() {
             <Nav2 type="Popular Titles"/>
                 <div className="body">
                     <div onClick={()=>{history.push('/movies')}}>
-                        <Card url="https://raw.githubusercontent.com/nik-1207/nik-1207/main/Movie.png"alt="movie icon" title="Popular Movies"/>
+                        <Card url="../clapperboard.png"alt="movie icon" title="Popular Movies"/>
                     </div>
                     <div onClick={()=>{history.push('/series')}}>
-                        <Card url="https://raw.githubusercontent.com/nik-1207/nik-1207/main/series.png"alt="series icon" title="Popular series"/>
+                        <Card url="../clapperboard.png"alt="series icon" title="Popular series"/>
                     </div>
                 </div>
         </div>
